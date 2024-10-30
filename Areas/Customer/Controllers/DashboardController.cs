08c988f2742d87f5335f9131dc8235c9a55ee954
AdamJC-Git <52371823+AdamJC-Git@users.noreply.github.com>
2024-10-30 14:45:03 +0000

    Add files via upload

---
+++ Areas/Customer/Controllers/DashboardController.cs
@@ -0,0 +1,33 @@
+﻿using GadgetCompany.DataAccess;
+using GadgetCompany.Models;
+using GadgetCompany.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace GadgetCompany.Areas.Customer.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public DashboardController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index(ProductFilter filter)
+        {
+            var productVm = new ProductVM();
+
+            //will just display Products from this page as it is the default landing page at the moment
+            var products = _db.Products.Include(x => x.ProductType);
+
+            if (!string.IsNullOrWhiteSpace(filter.ProductName))
+            {
+                products = products.Where(x => x.Name.Contains(filter.ProductName)).Include(y => y.ProductType);
+            }
+            productVm.Products = products;
+            
+            return View(productVm);
+        }
+    }
+}
---
+++ Areas/Customer/Controllers/ShoppingCartController.cs
@@ -0,0 +1,38 @@
+﻿using GadgetCompany.DataAccess;
+using GadgetCompany.Models;
+using GadgetCompany.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace GadgetCompany.Areas.Customer.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public ShoppingCartController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        //public IActionResult Index(int productId)
+        //{
+        //Get all the shopping cart records for a specific system user
+        //Run the GetTotalCost method
+
+        //Display users shopping cart
+
+
+        //return View(cartVm);
+        //}
+
+        //Other methods needed
+
+        //Add product - increase the count of the product in shopping cart and run GetTotalCost
+
+        //Minus product - decrease the count of the product in shopping cart and run GetTotalCost
+
+        //Remove product - completely delete the product from user's shopping cart and run GetTotalCost
+
+        //GetTotalCost method to add the cost of the shopping cart and display to user.
+    }
+}
+
